df18fcb98dfb98f4a75bd5965ac7c17c5cf0efd5
consoleLogIt <67229376+consoleLogIt@users.noreply.github.com>
2020-10-29 06:46:59 -0700

    general modification 1

--- src/Components/Charts/Chart.jsx
+++ src/Components/Charts/Chart.jsx
@@ -91,16 +91,16 @@ function Chart({data:{confirmed,active,recovered,deaths},State}) {
          datasets :[{
          data:[active,confirmed,recovered,deaths],
          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
+             'rgba(255, 206, 86, 0.2)',
             'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',  
+            'rgba(10, 210, 10, 0.2)',
+             'rgba(255, 99, 132, 0.2)',
          ],
          borderColor: [
-            'rgba(255, 99, 132, 1)',
+           'rgba(255, 206, 10, 1)',
             'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
+            'rgba(50, 192, 45, 1)',
+             'rgba(255, 99, 132, 1)',
          ],
          borderWidth:1,
         }
