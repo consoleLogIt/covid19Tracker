4ad746f93931c077d7d600488893bf687365b55c
consoleLogIt <67229376+consoleLogIt@users.noreply.github.com>
2020-10-29 05:14:02 -0700

    Updates

---
+++ precache-manifest.893e1f90e65a901f402b870e47d914ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a008fe09eeaf42f3f01979018b0e37",
+    "url": "/covid19Tracker/index.html"
+  },
+  {
+    "revision": "345c682962aac53cdc50",
+    "url": "/covid19Tracker/static/css/main.5a07fb32.chunk.css"
+  },
+  {
+    "revision": "81d017f3eedf2cb482d6",
+    "url": "/covid19Tracker/static/js/2.02df0cf8.chunk.js"
+  },
+  {
+    "revision": "cd6e711b9e263c2324169127d95e8bd1",
+    "url": "/covid19Tracker/static/js/2.02df0cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "345c682962aac53cdc50",
+    "url": "/covid19Tracker/static/js/main.b6c549b5.chunk.js"
+  },
+  {
+    "revision": "aa69968d7ad22f3201da",
+    "url": "/covid19Tracker/static/js/runtime-main.65a99924.js"
+  },
+  {
+    "revision": "01cc6a34420a4b38d0ba5713544f184c",
+    "url": "/covid19Tracker/static/media/covid19.01cc6a34.svg"
+  },
+  {
+    "revision": "f3a60ba3f717eca3ed511426a1debc55",
+    "url": "/covid19Tracker/static/media/indian-flag.f3a60ba3.svg"
+  }
+]);
